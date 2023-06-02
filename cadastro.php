68d13624a09dc87a5b9c7adf6560842daa7148a0
M4Noname <127414613+M4Noname@users.noreply.github.com>
2023-06-02 08:36:51 -0300

    css+correção de bugs+comentarios

--- cadastro.php
+++ cadastro.php
@@ -6,13 +6,13 @@ $sobrenome=$_POST['sobrenome'];
 $email=$_POST['email'];
 $senha=$_POST['senha'];
 
-$sql="INSERT INTO user(nome,sobrenome,email,senha)value('$nome,$sobrenome,$email,$senha')";
+$sql="INSERT INTO infolsd(nome,sobrenome,email,senha)value('$nome,$sobrenome,$email,$senha')";
 
 if(mysqli_query($conect,$sql)){
     echo"deu foi booooooom boy:";
 }
 else{
-    echo"erro...... sei la kk:".mysqli_connect_error($conect);
+    echo"erro...... sei la kk:".mysqli_connect_error();
 
 }
 
--- conect.php
+++ conect.php
@@ -5,7 +5,7 @@ $user="root";
 $sen="";
 $bd="infolsd";
 
-$conect=mysqli_connect($host,$user,$sen,$bd);
+$conect = mysqli_connect($host,$user,$sen,$bd);
 
 if(!$conect){
     echo"houve um erro:".mysqli_connect_error();
